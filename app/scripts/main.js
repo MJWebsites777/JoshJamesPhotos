7d9c81ea0bbbe89374c618394b0dfff120f01748
MJWebsites777 <mjjwebsites@gmail.com>
2015-03-09 11:58:12 -0500

    removed unwanted code and comments to make way for new design

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,51 +1,12 @@
 //TODO
-//change url hash on page change?
-//remove headings from nav and add background images to show user where that nav takes them
 
 var currentPage = 'home', lastPage = '';
 $(document).ready(function() {
-	var page = new Page();
-
-	onload=onresize=function(){	
-		switch (currentPage){
-			case 'gallery': 
-				$('.nav1').height($(window).height()/2);
-				break;
-			case 'bio': 
-				$('.nav2').height($(window).height()/2);
-				break;
-			case 'contact': 
-				$('.nav3').height($(window).height()/2);
-				break;
-			default:
-				break;
-		}
-		//$('.bgImg').height($(window).height());
-		//resizeBG();
-	};
+	slideShow.start();
 
 //Clicks
-	$(document).on('click', '.nav1', function(event){
-		var target = $(event.target);
-		if (target.attr('class') === 'nav1 navHover' || event.target.nodeName === 'H1'){
-			page.gotoGallery();
-		}
-		
-	});
-	$(document).on('click', '.nav2', function(event){
-		var target = $(event.target);
-		if (target.attr('class') === 'nav2 navHover' || event.target.nodeName === 'H1'){
-			page.gotoBio();
-		}
-	});
-	$(document).on('click', '.nav3', function(event){
-		var target = $(event.target);
-		if (target.attr('class') === 'nav3 navHover' || event.target.nodeName === 'H1'){
-			page.gotoContact();
-		}
-	});
 	$(document).on('click', '.logo', function(){
-		page.gotoHome();
+		//open bio information?
 	});
 //------
 	var img = new Image();
@@ -55,6 +16,4 @@ $(document).ready(function() {
 		$('.nav').css('opacity', '1');
 		slideShow.load();
 	};
-	//drawTiles('bg', $('.bgImg'));
-	//$('.tile').on('contextmenu', 'canvas', function(){ return false; });
 });
