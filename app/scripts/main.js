3c307f1b05972483a9aecf527c1e05cae29c01d6
MJWebsites777 <mjjwebsites@gmail.com>
2015-04-15 15:14:41 -0500

    added .bioContent mouseover event to open bio, added .hideBio click even to close bio

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,8 +5,37 @@ $(document).ready(function() {
 	slideShow.start();
 
 //Clicks
-	$(document).on('click', '.logo', function(){
-		//open bio information?
+	var isAnimating = false;
+	var bioOpen = false;
+	$(document).on('mouseover', '.bioContent', function(){
+		if (!isAnimating && !bioOpen){
+			var bio = $('.bio');
+			bio.css({
+				'transform': 'translateY(-120%)',
+				'cursor': 'default'
+			});
+			$('.bioOverlay').css('display', 'block');
+			setTimeout(function(){$('.bioOverlay').css('opacity', '1');}, 50);
+			slideShow.pause();
+			slideShow.focused = false;
+			bioOpen = true;
+		}
+	});
+	$(document).on('click', '.hideBio', function(){
+		var bio = $('.bio');
+		bio.css({
+			'transform': '',
+			'cursor': ''
+		});
+		isAnimating = true;
+		$('.bioOverlay').css('opacity', '');
+		setTimeout(function(){
+			$('.bioOverlay').css('display', ''); 
+			isAnimating = false;
+			bioOpen = false;
+		}, 350);
+		slideShow.resume();
+		slideShow.focused = true;
 	});
 //------
 	var img = new Image();
